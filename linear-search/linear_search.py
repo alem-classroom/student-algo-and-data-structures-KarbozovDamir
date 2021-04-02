a1fa71399abf716ff1970263a5a35d881b2ca671
Damir K <damirkap89@gmail.com>
2021-04-02 23:51:31 +0600

    initial commit

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -1,4 +1,9 @@
 def linear_search(lst, to_find):
+    for i in range(len(lst)):
+        if to_find == lst[i]:
+            return i
+        else:
+            return -1
   # search for the element to_find inside lst
   # if found, return index of element
   # else return -1
